d7525945311c86755d616ccde7aae545a2549106
jsr029 <jsr059@gmail.com>
2022-01-29 13:06:16 +0100

    Update

--- js/displayMenu.js
+++ js/displayMenu.js
@@ -1,24 +1,42 @@
 class displayMenu {
     render(data) {
         let dropIList = document.querySelector('.dropdown_ingredients-list');
-        let boxIMenu=[];
+        let globalSearch = document.querySelector('.globalSearch');
+        let boxIMenu = [];
         let uniqboxIMenu = [];
-        data.forEach(element => {
-            element.ingredients.forEach(elm => {
-                element.ingredients.sort(function (a, b) {
-                    if (a.ingredient < b.ingredient) {
-                        return -1;
-                    }
-                    if (a.ingredient > b.ingredient) {
-                        return 1;
-                    }
-                    return 0;
-                });
-                boxIMenu.push(`<a href="#">${elm.ingredient}</a>`);
-            });
-            uniqboxIMenu = [...new Set(boxIMenu)];
-            dropIList.innerHTML = uniqboxIMenu;
+        let ingredientsTabSort = [];
+        let ingredientsTab = [];
+        let dropAList = document.querySelector('.dropdown_appliance-list');
+        let globalSearchA = document.querySelector('.globalSearch');
+        let boxAMenu = [];
+        let uniqboxAMenu = [];
+        let applianceTabSort = [];
+        let applianceTab = [];
+        for (let i = 0; i < data.length; i++) {
+                applianceTab.push(data[i].appliance.toLowerCase());
+            for (let j = 0; j < data[i].ingredients.length; j++)
+                    ingredientsTab.push(data[i].ingredients[j].ingredient.toLowerCase());
+                  }
+        uniqboxIMenu = [...new Set(ingredientsTab)];
+        uniqboxIMenu.sort();
+        uniqboxIMenu.forEach(elm => {
+            boxIMenu.push(`<a href="javascript:void(0)">${elm}</a>`);
         });
+        dropIList.innerHTML = boxIMenu.join('');
+        uniqboxIMenu = [...new Set(ingredientsTab)];
+        uniqboxIMenu.sort();
+        uniqboxIMenu.forEach(elm => {
+            boxIMenu.push(`<a href="javascript:void(0)">${elm}</a>`);
+        });
+        dropIList.innerHTML = boxIMenu.join('');
+        /** Appliance */
+        uniqboxAMenu = [...new Set(applianceTab)];
+        uniqboxAMenu.sort();
+        uniqboxAMenu.forEach(elm => {
+            boxAMenu.push(`<a href="javascript:void(0)">${elm}</a>`);
+        });
+        dropAList.innerHTML = boxAMenu.join('');
     }
+
 }
 export default displayMenu;
--- js/dropdown.js
+++ js/dropdown.js
@@ -1,31 +1,52 @@
 class dropdown {
     render() {
-        const iconeIngredients = document.querySelector('.dropdown_ingredients .fa-angle-up');
+        let iconeIngredients = document.querySelector('.dropdown_ingredients .fa-angle-up');
         iconeIngredients.addEventListener('click', openIDrop);
-        const iconeAppliance = document.querySelector('.dropdown_appliance .fa-angle-up');
+        let iconeAppliance = document.querySelector('.dropdown_appliance .fa-angle-up');
         iconeAppliance.addEventListener('click', openADrop);
-        const iconeUstensils = document.querySelector('.dropdown_ustensils .fa-angle-up');
+        let iconeUstensils = document.querySelector('.dropdown_ustensils .fa-angle-up');
         iconeUstensils.addEventListener('click', openUDrop);
     }
 }
 let openIDrop = function (iClicked) {
     let iClickedClassName = iClicked.target.className;
     let iClickedClassNameSplit = iClickedClassName.split(' ')[1];
-    const dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
-    const dropIngredientsBloc = document.querySelector('.dropdown_ingredients-input');
-    const dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
-    const dropIH2 = document.querySelector('.dropdown_ingredients h2');
+    let dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
+    let dropIngredientsBloc = document.querySelector('.dropdown_ingredients-input');
+    let dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
+    let dropIH2 = document.querySelector('.dropdown_ingredients h2');
+    let ingredientsList = document.querySelector('.dropdown_ingredients-list');
+    /**Appliance */
+    let dropAH2 = document.querySelector('.dropdown_appliance h2');
+    let dropAH2span = document.querySelector('.dropdown_appliance h2 span');
+    let aClicked = document.querySelector('.dropdown_appliance-input i');
+    let dropAppliance = document.querySelector('.dropdown_appliance-input .applianceSearch');
+    let applianceList = document.querySelector('.dropdown_appliance-list');
+    let dropAH2i = document.querySelector('.dropdown_appliance h2 i');
+    /**Appliance End */
     if (iClickedClassNameSplit == 'fa-angle-up') {
         dropIngredients.style.display = 'block';
         dropIngredients.style.minWidth = '270px';
+        ingredientsList.style.display = 'grid';
         iClicked.target.classList.remove('fa-angle-up');
         iClicked.target.classList.add('fa-angle-down');
         dropIH2span.style.display = 'none';
         dropIngredientsBloc.appendChild(iClicked.target);
-    }
-    else if(iClickedClassNameSplit == 'fa-angle-down'){
+        
+        dropAppliance.style.display = 'none';
+        applianceList.style.display = 'none';
+        if(aClicked){
+            aClicked.className = 'fa fa-angle-up';
+            dropAH2.appendChild(aClicked);
+        }
+        else if(!aClicked){
+            dropAH2i.className = 'fa fa-angle-up';
+        }
+        dropAH2span.style.display = 'block';
+    } else if (iClickedClassNameSplit == 'fa-angle-down') {
         dropIngredients.style.display = 'none';
         dropIngredients.style.minWidth = '200px';
+        ingredientsList.style.display = 'none';
         iClicked.target.classList.remove('fa-angle-down');
         iClicked.target.classList.add('fa-angle-up');
         dropIH2span.style.display = 'block';
@@ -35,19 +56,41 @@ let openIDrop = function (iClicked) {
 let openADrop = function (aClicked) {
     let aClickedClassName = aClicked.target.className;
     let aClickedClassNameSplit = aClickedClassName.split(' ')[1];
-    const dropAppliance = document.querySelector('.dropdown_appliance-input .applianceSearch');
-    const dropApplianceBloc = document.querySelector('.dropdown_appliance-input');
-    const dropAH2span = document.querySelector('.dropdown_appliance h2 span');
-    const dropAH2 = document.querySelector('.dropdown_appliance h2');
+    let dropAppliance = document.querySelector('.container .dropdown_appliance-input .applianceSearch');
+    let dropApplianceBloc = document.querySelector('.dropdown_appliance-input');
+    let dropAH2span = document.querySelector('.dropdown_appliance h2 span');
+    let dropAH2 = document.querySelector('.dropdown_appliance h2');
+    let applianceList = document.querySelector('.dropdown_appliance-list');
+    /**Ingredients */
+    let dropIH2 = document.querySelector('.dropdown_ingredients h2');
+    let dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
+    let iClicked = document.querySelector('.dropdown_ingredients-input i');
+    let dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
+    let ingredientsList = document.querySelector('.dropdown_ingredients-list');
+    let dropIH2i = document.querySelector('.dropdown_ingredients h2 i');
+    /**Ingredients end */
     if (aClickedClassNameSplit == 'fa-angle-up') {
         dropAppliance.style.display = 'block';
+        applianceList.style.display = 'grid';
         aClicked.target.classList.remove('fa-angle-up');
         aClicked.target.classList.add('fa-angle-down');
         dropAH2span.style.display = 'none';
         dropApplianceBloc.appendChild(aClicked.target);
-    }
-    else if(aClickedClassNameSplit == 'fa-angle-down'){
+        /**Ingredients */
+        dropIngredients.style.display = 'none';
+        ingredientsList.style.display = 'none';
+        if(iClicked){
+            iClicked.className = 'fa fa-angle-up';
+            dropIH2.appendChild(iClicked);
+        }
+        else if(!iClicked){
+            dropIH2i.className = 'fa fa-angle-up';
+        }
+        dropIH2span.style.display = 'block';
+        /**Ingredients end */
+    } else if (aClickedClassNameSplit == 'fa-angle-down') {
         dropAppliance.style.display = 'none';
+        applianceList.style.display = 'none';
         aClicked.target.classList.remove('fa-angle-down');
         aClicked.target.classList.add('fa-angle-up');
         dropAH2span.style.display = 'block';
@@ -55,20 +98,13 @@ let openADrop = function (aClicked) {
     }
 };
 let openUDrop = function (uClicked) {
-    let uClickedClassName = uClicked.target.className;
-    let uClickedClassNameSplit = uClickedClassName.split(' ')[1];
-    const dropUstensils = document.querySelector('.dropdown_ustensils-input .ustensilSearch');
-    const dropUstensilsBloc = document.querySelector('.dropdown_ustensils-input');
-    const dropUH2span = document.querySelector('.dropdown_ustensils h2 span');
-    const dropUH2 = document.querySelector('.dropdown_ustensils h2');
     if (uClickedClassNameSplit == 'fa-angle-up') {
         dropUstensils.style.display = 'block';
         uClicked.target.classList.remove('fa-angle-up');
         uClicked.target.classList.add('fa-angle-down');
         dropUH2span.style.display = 'none';
         dropUstensilsBloc.appendChild(uClicked.target);
-    }
-    else if(uClickedClassNameSplit == 'fa-angle-down'){
+    } else if (uClickedClassNameSplit == 'fa-angle-down') {
         dropUstensils.style.display = 'none';
         uClicked.target.classList.remove('fa-angle-down');
         uClicked.target.classList.add('fa-angle-up');
