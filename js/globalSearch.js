36daf4ca545f8b7da5981cd0537c38b6637dd97e
jsr029 <jsr059@gmail.com>
2022-02-04 13:43:18 +0100

    Update globalSearch Algo 2

--- js/globalSearch.js
+++ js/globalSearch.js
@@ -19,48 +19,33 @@ class globalSearch {
                 applianceResult = [];
                 ingredientsResult = [];
                 ustensilsResult = [];
-                baseModified = data.map(item => {
-                    const base = {};
-                    base.id = item.id;
-                    base.name = item.name;
-                    base.servings = item.servings;
-                    base.time = item.time;
-                    base.appliance = item.appliance;
-                    base.ustensils = item.ustensils;
-                    base.description = item.description;
-                    base.ingredients = item.ingredients.map(it => {
-                        const ing = {};
-                        ing.ingredient = it.ingredient;
-                        ing.unit = it.unit ? it.unit : '';
-                        ing.quantity = it.quantity;
-                        return ing;
-                    });
-                    return base;
-                });
-                baseModified.forEach(elm => {
-                    elm.ingredients.filter(function (ing) {
-                        if (ing.ingredient.toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
-                            ingredientsResult.push(elm);
-                        }
-                    });
-                    elm.ustensils.filter(function (ust) {
-                        if (ust.toLowerCase().includes(globalInput.toLowerCase())) {
-                            ustensilsResult.push(elm);
-                        }
-                    });
-                    if (elm.appliance.toLowerCase().includes(globalInput.toLowerCase())) {
-                        applianceResult.push(elm);
+                for (let i = 0; i < recipes.length; i++) {
+                    if (recipes[i].name.toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
+                        nameResult.push(recipes[i]);
                     }
-                    if (elm.name.toLowerCase().includes(globalInput.toLowerCase())) {
-                        nameResult.push(elm);
+                    if (recipes[i].description.toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
+                        descriptionResult.push(recipes[i]);
                     }
+                    if (recipes[i].appliance.toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
+                        applianceResult.push(recipes[i]);
+                    }
+                    for (let k = 0; k < recipes[i].ingredients.length; k++) {
+                        if (recipes[i].ingredients[k].ingredient.toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
+                            ingredientsResult.push(recipes[i]);
+                        }
+                    }
+                    for (let k = 0; k < recipes[i].ustensils.length; k++) {
+                        if (recipes[i].ustensils[k].toLowerCase().trim().includes(globalInput.toLowerCase().trim())) {
+                            ustensilsResult.push(recipes[i]);
+                        }
+                    }
+                }
                     newBaseFilteredConcated = [...new Set([...nameResult, ...ingredientsResult, ...descriptionResult, ...applianceResult, ...ustensilsResult])];
                     newBaseFilteredConcated.sort((a, b) => b.name.localeCompare(a.name));
                     new displayRecipes().removeMessage();
                     new displayRecipes().nbrRecipes(newBaseFilteredConcated);
                     new displayRecipes().render(newBaseFilteredConcated);
                     new displayMenu().render(newBaseFilteredConcated, globalInput);
-                });
             } else {
                 new displayRecipes().removeMessage();
                 recepiesSort = data.sort((a, b) => b.name.localeCompare(a.name));
