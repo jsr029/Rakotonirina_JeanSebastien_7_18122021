6d5914b8492ee70fd8f59953a02448d069be36dc
jsr029 <jsr059@gmail.com>
2022-02-09 01:34:36 +0100

    Update

--- js/globalSearch.js
+++ js/globalSearch.js
@@ -40,17 +40,13 @@ class globalSearch {
                         }
                     }
                 }
-                    newBaseFilteredConcated = [...new Set([...nameResult, ...ingredientsResult, ...descriptionResult, ...applianceResult, ...ustensilsResult])];
-                    newBaseFilteredConcated.sort((a, b) => b.name.localeCompare(a.name));
-                    new displayRecipes().removeMessage();
-                    new displayRecipes().nbrRecipes(newBaseFilteredConcated);
-                    new displayRecipes().render(newBaseFilteredConcated);
-                    new displayMenu().render(newBaseFilteredConcated, globalInput);
-            } else {
+                newBaseFilteredConcated = [...new Set([...nameResult, ...ingredientsResult, ...descriptionResult, ...applianceResult, ...ustensilsResult])];
+                newBaseFilteredConcated.sort((a, b) => b.name.localeCompare(a.name));
                 new displayRecipes().removeMessage();
-                recepiesSort = data.sort((a, b) => b.name.localeCompare(a.name));
-                new displayRecipes().render(recepiesSort);
-                new displayMenu().render(recepiesSort);
+                new displayRecipes().nbrRecipes(newBaseFilteredConcated);
+                new displayRecipes().render(newBaseFilteredConcated);
+                new displayMenu().render(newBaseFilteredConcated, globalInput);
+
             }
         });
         recepiesSort = data.sort((a, b) => b.name.localeCompare(a.name));
