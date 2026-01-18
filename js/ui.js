6e524ea070097effd9a6dfd6629f329859264ee7
jsr029 <jsr059@gmail.com>
2026-01-18 13:58:24 +0100

    po

--- js/ui.js
+++ js/ui.js
@@ -22,7 +22,7 @@ function refreshRecipesDisplay(filtered) {
                     </ul>
                 </div>
             `;
-            grid ? grid.appendChild(article) :'';
+            grid ? grid.appendChild(article) : null;
         });
     }
 
