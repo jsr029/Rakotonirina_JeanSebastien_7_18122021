ff708c5bc1905bb60f3f25e6755a8e0b9d8db81d
Jean-Sebastien Rakotonirina <78790620+jsr029@users.noreply.github.com>
2026-01-19 21:13:23 +0100

    Refactor Dropdown class for better maintainability
    Refactored dropdown functionality to simplify event handling and improve code structure.

--- js/Dropdown.js
+++ js/Dropdown.js
@@ -1,244 +1,87 @@
-class dropdown {
-    render() {
-        let iconeIngredients = document.querySelector('.dropdown_ingredients .fa-angle-down');
-        iconeIngredients.addEventListener('click', openIDrop);
-        let iconeAppliance = document.querySelector('.dropdown_appliance .fa-angle-down');
-        iconeAppliance.addEventListener('click', openADrop);
-        let iconeUstensils = document.querySelector('.dropdown_ustensils .fa-angle-down');
-        iconeUstensils.addEventListener('click', openUDrop);
-    }
-}
-let openIDrop = function (iClicked) {
-    let iClickedClassName = iClicked.target.className;
-    let iClickedClassNameSplit = iClickedClassName.split(' ')[1];
-    let dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
-    let dropIngredientsBloc = document.querySelector('.dropdown_ingredients-input');
-    let dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
-    let dropIH2 = document.querySelector('.dropdown_ingredients h2');
-    let ingredientsList = document.querySelector('.dropdown_ingredients-list');
-    let ustensilsList = document.querySelector('.dropdown_ustensils-list');
-    let globalInput = document.querySelector('.globalSearch');
-    /**Appliance */
-    let dropAH2 = document.querySelector('.dropdown_appliance h2');
-    let dropAH2span = document.querySelector('.dropdown_appliance h2 span');
-    let aClicked = document.querySelector('.dropdown_appliance-input i');
-    let dropAppliance = document.querySelector('.dropdown_appliance-input .applianceSearch');
-    let applianceList = document.querySelector('.dropdown_appliance-list');
-    let dropAH2i = document.querySelector('.dropdown_appliance h2 i');
-    /**Appliance End */
-    /**Ustensils */
-    let dropUstensils = document.querySelector('.dropdown_ustensils-input .ustensilSearch');
-    let uClicked = document.querySelector('.dropdown_ustensils-input i');
-    let dropUH2i = document.querySelector('.dropdown_ustensils h2 i');
-    let dropUH2 = document.querySelector('.dropdown_ustensils h2');
-    let dropUH2span = document.querySelector('.dropdown_ustensils h2 span');
-    /**Ustensils end */
-    if (iClickedClassNameSplit == 'fa-angle-down') {
-        dropIngredients.style.display = 'block';
-        dropIngredients.style.minWidth = '270px';
-        ingredientsList.style.display = 'grid';
-        iClicked.target.classList.remove('fa-angle-down');
-        iClicked.target.classList.add('fa-angle-up');
-        dropIH2span.style.display = 'none';
-        dropIH2.style.display = 'none';
-        dropIngredientsBloc.appendChild(iClicked.target);
-        /**Appliance */
-        dropAppliance.style.display = 'none';
-        applianceList.style.display = 'none';
-        if (aClicked) {
-            aClicked.className = 'fa fa-angle-down';
-            dropAH2.appendChild(aClicked);
-        } else if (!aClicked) {
-            dropAH2i.className = 'fa fa-angle-down';
-        }
-        dropAH2span.style.display = 'block';
-        /**Appliance end */
-        /**Ustensils */
-        dropUstensils.style.display = 'none';
-        ustensilsList.style.display = 'none';
-        if (uClicked) {
-            uClicked.className = 'fa fa-angle-down';
-            dropUH2.appendChild(uClicked);
-        } else if (!uClicked) {
-            dropUH2i.className = 'fa fa-angle-down';
-        }
-        dropUH2span.style.display = 'block';
-        dropAH2.style.display = 'flex';
-        dropAH2span.style.display = 'block';
-        /**Ustensils end */
-        dropUH2.style.display = 'flex';
-        dropUH2span.style.display = 'block';
-        globalInput.addEventListener('focus', function (event) {
-            globalInput.blur();
-        });
-    } else if (iClickedClassNameSplit == 'fa-angle-up') {
-        dropIngredients.style.display = 'none';
-        dropIngredients.style.minWidth = '200px';
-        ingredientsList.style.display = 'none';
-        iClicked.target.classList.remove('fa-angle-up');
-        iClicked.target.classList.add('fa-angle-down');
-        dropUH2.style.display = 'flex';
-        dropUH2span.style.display = 'block';
-        dropAH2.style.display = 'flex';
-        dropAH2span.style.display = 'block';
-        dropIH2span.style.display = 'block';
-        dropIH2.style.display = 'flex';
-        dropIH2.appendChild(iClicked.target);
-        globalInput.addEventListener('focus', function (event) {
-            globalInput.focus();
-        });
-    }
-};
-let openADrop = function (aClicked) {
-    let aClickedClassName = aClicked.target.className;
-    let aClickedClassNameSplit = aClickedClassName.split(' ')[1];
-    let dropAppliance = document.querySelector('.container .dropdown_appliance-input .applianceSearch');
-    let dropApplianceBloc = document.querySelector('.dropdown_appliance-input');
-    let dropAH2span = document.querySelector('.dropdown_appliance h2 span');
-    let dropAH2 = document.querySelector('.dropdown_appliance h2');
-    let applianceList = document.querySelector('.dropdown_appliance-list');
-    let globalInput = document.querySelector('.globalSearch');
-    /**Ingredients */
-    let dropIH2 = document.querySelector('.dropdown_ingredients h2');
-    let dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
-    let iClicked = document.querySelector('.dropdown_ingredients-input i');
-    let dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
-    let ingredientsList = document.querySelector('.dropdown_ingredients-list');
-    let dropIH2i = document.querySelector('.dropdown_ingredients h2 i');
-    /**Ingredients end */
-    /**Ustensils */
-    let dropUH2 = document.querySelector('.dropdown_ustensils h2');
-    let dropUH2span = document.querySelector('.dropdown_ustensils h2 span');
-    let uClicked = document.querySelector('.dropdown_ustensils-input i');
-    let dropUstensils = document.querySelector('.dropdown_ustensils-input .ustensilSearch');
-    let ustensilsList = document.querySelector('.dropdown_ustensils-list');
-    let dropUH2i = document.querySelector('.dropdown_ustensils h2 i');
-    /**Ustensils end */
-    if (aClickedClassNameSplit == 'fa-angle-down') {
-        dropAppliance.style.display = 'block';
-        applianceList.style.display = 'grid';
-        aClicked.target.classList.remove('fa-angle-down');
-        aClicked.target.classList.add('fa-angle-up');
-        dropAH2span.style.display = 'none';
-        dropAH2.style.display = 'none';
-        dropApplianceBloc.appendChild(aClicked.target);
-        /**Ingredients */
-        dropIngredients.style.display = 'none';
-        ingredientsList.style.display = 'none';
-        dropIH2.style.display = 'none';
-        if (iClicked) {
-            iClicked.className = 'fa fa-angle-down';
-            dropIH2.style.display = 'flex';
-            dropIH2.appendChild(iClicked);
-        } else if (!iClicked) {
-            dropIH2.style.display = 'flex';
-            dropIH2i.className = 'fa fa-angle-down';
-        }
-        dropIH2span.style.display = 'block';
-        /**Ingredients end */
-        /**Ustensils */
-        dropUstensils.style.display = 'none';
-        ustensilsList.style.display = 'none';
-        if (uClicked) {
-            uClicked.className = 'fa fa-angle-down';
-            dropUH2.appendChild(uClicked);
-        } else if (!uClicked) {
-            dropUH2i.className = 'fa fa-angle-down';
-        }
-        dropUH2span.style.display = 'block';
-        dropUH2.style.display = 'flex';
-        dropUH2span.style.display = 'block';
-        /**Ustensils end */
-        globalInput.addEventListener('focus', function (event) {
-            globalInput.blur();
-        });
-    } else if (aClickedClassNameSplit == 'fa-angle-up') {
-        dropAppliance.style.display = 'none';
-        applianceList.style.display = 'none';
-        aClicked.target.classList.remove('fa-angle-up');
-        aClicked.target.classList.add('fa-angle-down');
-        dropUH2.style.display = 'flex';
-        dropUH2span.style.display = 'block';
-        dropAH2.style.display = 'flex';
-        dropAH2span.style.display = 'block';
-        dropAH2.appendChild(aClicked.target);
-    }
-};
-let openUDrop = function (uClicked) {
-    let uClickedClassName = uClicked.target.className;
-    let uClickedClassNameSplit = uClickedClassName.split(' ')[1];
-    let dropUstensils = document.querySelector('.dropdown_ustensils-input .ustensilSearch');
-    let dropUstensilsBloc = document.querySelector('.dropdown_ustensils-input');
-    let dropUH2span = document.querySelector('.dropdown_ustensils h2 span');
-    let dropUH2 = document.querySelector('.dropdown_ustensils h2');
-    let ustensilsList = document.querySelector('.dropdown_ustensils-list');
-    let globalInput = document.querySelector('.globalSearch');
-    /**Ingredients */
-    let dropIH2 = document.querySelector('.dropdown_ingredients h2');
-    let dropIH2span = document.querySelector('.dropdown_ingredients h2 span');
-    let iClicked = document.querySelector('.dropdown_ingredients-input i');
-    let dropIngredients = document.querySelector('.dropdown_ingredients-input .ingredientSearch');
-    let ingredientsList = document.querySelector('.dropdown_ingredients-list');
-    let dropIH2i = document.querySelector('.dropdown_ingredients h2 i');
-    /**Ingredients end */
-    /**Appliance */
-    let dropAH2 = document.querySelector('.dropdown_appliance h2');
-    let dropAH2span = document.querySelector('.dropdown_appliance h2 span');
-    let aClicked = document.querySelector('.dropdown_appliance-input i');
-    let dropAppliance = document.querySelector('.dropdown_appliance-input .applianceSearch');
-    let applianceList = document.querySelector('.dropdown_appliance-list');
-    let dropAH2i = document.querySelector('.dropdown_appliance h2 i');
-    /**Appliance End */
-    if (uClickedClassNameSplit == 'fa-angle-down') {
-        dropUstensils.style.display = 'block';
-        ustensilsList.style.display = 'grid';
-        uClicked.target.classList.remove('fa-angle-down');
-        uClicked.target.classList.add('fa-angle-up');
-        dropUH2span.style.display = 'none';
-        dropUstensilsBloc.appendChild(uClicked.target);
-        /**Ingredients */
-        dropIngredients.style.display = 'none';
-        ingredientsList.style.display = 'none';
-        if (iClicked) {
-            iClicked.className = 'fa fa-angle-down';
-            dropIH2.style.display = 'flex';
-            dropIH2.appendChild(iClicked);
-        } else if (!iClicked) {
-            dropIH2.style.display = 'flex';
-            dropIH2i.className = 'fa fa-angle-down';
-        }
-        dropIH2span.style.display = 'block';
-        /**Ingredients end */
-        /**Appliance */
-        dropAppliance.style.display = 'none';
-        applianceList.style.display = 'none';
-        if (aClicked) {
-            aClicked.className = 'fa fa-angle-down';
-            dropAH2.appendChild(aClicked);
-        } else if (!aClicked) {
-            dropAH2i.className = 'fa fa-angle-down';
-        }
-        dropAH2span.style.display = 'block';
-        dropAH2.style.display = 'flex';
-        dropAH2span.style.display = 'block';
-        dropUH2.style.display = 'none';
-        dropUH2span.style.display = 'none';
-        /**Appliance end */
-        globalInput.addEventListener('focus', function (event) {
-            globalInput.blur();
-        });
-    } else if (uClickedClassNameSplit == 'fa-angle-up') {
-        dropUstensils.style.display = 'none';
-        uClicked.target.classList.remove('fa-angle-up');
-        uClicked.target.classList.add('fa-angle-down');
-        dropUH2.style.display = 'flex';
-        dropUH2span.style.display = 'block';
-        dropAH2.style.display = 'flex';
-        dropAH2span.style.display = 'block';
-        dropUH2span.style.display = 'block';
-        dropUH2.appendChild(uClicked.target);
-        ustensilsList.style.display = 'none';
-    }
-};
+// Dropdown.js (version simplifiée et sans récursion infinie)
 
-export default dropdown;
+class Dropdown {
+  constructor() {
+    this.dropdowns = [
+      { 
+        container: '.dropdown_ingredients',
+        input: '.ingredientSearch',
+        list: '.dropdown_ingredients-list',
+        h2: '.dropdown_ingredients h2',
+        icon: '.dropdown_ingredients h2 i',
+        title: 'Ingrédients'
+      },
+      { 
+        container: '.dropdown_appliance',
+        input: '.applianceSearch',
+        list: '.dropdown_appliance-list',
+        h2: '.dropdown_appliance h2',
+        icon: '.dropdown_appliance h2 i',
+        title: 'Appareil'
+      },
+      { 
+        container: '.dropdown_ustensils',
+        input: '.ustensilSearch',
+        list: '.dropdown_ustensils-list',
+        h2: '.dropdown_ustensils h2',
+        icon: '.dropdown_ustensils h2 i',
+        title: 'Ustensiles'
+      }
+    ];
+  }
+
+  render() {
+    document.querySelector('.dropdown').addEventListener('click', (e) => {
+      const icon = e.target.closest('h2 i');
+      if (!icon) return;
+
+      const container = icon.closest('.dropdown_ingredients, .dropdown_appliance, .dropdown_ustensils');
+      if (!container) return;
+
+      this.toggleDropdown(container);
+    });
+  }
+
+  toggleDropdown(clickedContainer) {
+    const isOpen = clickedContainer.querySelector('i').classList.contains('fa-angle-up');
+
+    // Fermer TOUS les dropdowns d'abord
+    this.closeAllDropdowns();
+
+    // Puis ouvrir celui qu'on vient de cliquer (si il n'était pas déjà ouvert)
+    if (!isOpen) {
+      const input = clickedContainer.querySelector('input');
+      const list = clickedContainer.querySelector('[class$="-list"]');
+      const h2 = clickedContainer.querySelector('h2');
+      const span = h2.querySelector('span');
+      const icon = h2.querySelector('i');
+
+      input.style.display = 'block';
+      list.style.display = 'grid';
+      h2.style.display = 'none'; // ou flex selon ton design
+
+      icon.classList.remove('fa-angle-down');
+      icon.classList.add('fa-angle-up');
+    }
+  }
+
+  closeAllDropdowns() {
+    this.dropdowns.forEach(d => {
+      const container = document.querySelector(d.container);
+      const input = container.querySelector('input');
+      const list = container.querySelector('[class$="-list"]');
+      const h2 = container.querySelector('h2');
+      const span = h2.querySelector('span');
+      const icon = h2.querySelector('i');
+
+      input.style.display = 'none';
+      list.style.display = 'none';
+      h2.style.display = 'flex';
+      span.style.display = 'inline';
+      icon.classList.remove('fa-angle-up');
+      icon.classList.add('fa-angle-down');
+    });
+  }
+}
+
+export default Dropdown;
